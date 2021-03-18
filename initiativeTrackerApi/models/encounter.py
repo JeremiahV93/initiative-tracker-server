8b25c5ef07eef64b0c32ff341b3cf627ce83406c
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2021-03-17 20:11:02 -0500

    Added validation on the encounter model and Fkey

--- initiativeTrackerApi/models/encounter.py
+++ initiativeTrackerApi/models/encounter.py
@@ -3,17 +3,7 @@ from django.conf import settings
 from django.db.models.deletion import CASCADE
 
 class Encounter(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=CASCADE)
-    name = models.CharField(max_length=75)
-    roomcode = models.CharField( unique=True ,max_length=4)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    name = models.CharField(null=False, max_length=75)
+    roomcode = models.CharField(unique=True, max_length=4)
     archive = models.BooleanField(default=False)
-
-    # def generate_room_codes(length):
-    #     chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    #     code_list = [''.join(i) for i in product(chars, repeat=length)]
-    #     return code_list
-
-    # @property
-    # def roomcode(self):
-    #      roomcode = self.generate_room_codes(4)
-    #      return roomcode
