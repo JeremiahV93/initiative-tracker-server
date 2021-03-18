29ec13c2948467c2891776cff09ca8a714b5b69d
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2021-03-17 19:25:50 -0500

    Added try level for room code creation

--- initiativeTrackerApi/models/encounter.py
+++ initiativeTrackerApi/models/encounter.py
@@ -5,7 +5,7 @@ from django.db.models.deletion import CASCADE
 class Encounter(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=CASCADE)
     name = models.CharField(max_length=75)
-    roomcode = models.CharField(max_length=4)
+    roomcode = models.CharField( unique=True ,max_length=4)
     archive = models.BooleanField(default=False)
 
     # def generate_room_codes(length):
