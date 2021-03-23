a1847b194a45a7c157caded7eb9338263b7d7902
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2021-03-22 21:35:31 -0500

    Added pair deleteion

--- initiative_tracker_server/settings.py
+++ initiative_tracker_server/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '!^ym=v0cuy_ng3836esrepaf$^8+m&9u6*h05gg@zu!n&-78k^?'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
