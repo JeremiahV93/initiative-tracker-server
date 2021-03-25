26bb979ec8e7355e63347b522b6e45fb242bd652
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2021-03-25 09:57:56 -0500

    Heroku stuff

--- initiative_tracker_server/settings.py
+++ initiative_tracker_server/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!^ym=v0cuy_ng3836esrepaf$^8+m&9u6*h05gg@zu!n&-78k^?'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
