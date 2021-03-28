014a0121e7af865435604116990080ef309eb7dc
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2021-03-28 13:19:52 -0500

    removed duplicate code in settings.py

--- initiative_tracker_server/settings.py
+++ initiative_tracker_server/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'initiativeTrackerApi',
-    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -63,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'initiative_tracker_server.urls'
